f53fa5c128d9a280637c642e7ffb70dd53942251
minhye-lee <dlalsgp001@naver.com>
2022-01-20 23:45:04 +0900

    주석 추가

--- 2606/2606.js
+++ 2606/2606.js
@@ -2,10 +2,12 @@ let input = require('fs').readFileSync('temp.txt').toString().split('\n');
 console.log(input)
 const [num, cnt, ...rest] = input
 const arr = Array.from({length: Number(num)+1}, () => [])
+const result = Array.from({length: umber(num)+1}, () => 0)
+
 for(let i = 0; i < rest.length; i++) {
     const [a ,b] = (rest[i].split(' ').map((i) => Number(i)).sort((a,b) => a -b))
     console.log(a, b)
     arr[a].push(b)
-    // arr[b].push(a)
 }
-console.log(arr)
+
+
