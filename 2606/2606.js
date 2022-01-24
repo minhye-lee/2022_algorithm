04eadc2f99a229e7cfbf746461c09f2829cf9d42
minhye-lee <dlalsgp001@naver.com>
2022-01-24 19:48:44 +0900

    2606 solved

--- 2606/2606.js
+++ 2606/2606.js
@@ -1,13 +1,23 @@
-let input = require('fs').readFileSync('temp.txt').toString().split('\n');
-console.log(input)
+let input = require('fs').readFileSync('/dev/stdin', 'utf-8').trim().split('\n'); 
 const [num, cnt, ...rest] = input
 const arr = Array.from({length: Number(num)+1}, () => [])
-const result = Array.from({length: umber(num)+1}, () => 0)
+const result = Array.from({length: Number(num)+1}, () => 0)
 
 for(let i = 0; i < rest.length; i++) {
     const [a ,b] = (rest[i].split(' ').map((i) => Number(i)).sort((a,b) => a -b))
-    console.log(a, b)
     arr[a].push(b)
+    arr[b].push(a)
 }
 
-
+function dfs(index) {
+    arr[index].forEach((item) => {
+        console.log(item)
+        if(result[item]) {
+            return
+        }
+        result[item] = 1
+        dfs(item)
+    })
+}
+dfs(1)
+console.log(result.filter((i) => i === 1).length -1)
