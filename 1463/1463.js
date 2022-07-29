d3673e299697bf6f19cfd94021b604cee403f378
minhye-lee <dlalsgp001@naver.com>
2022-07-29 18:50:55 +0900

    1463 resolve

--- 1463/1463.js
+++ 1463/1463.js
@@ -1,15 +1,18 @@
-let input = Number(require('fs').readFileSync('temp.txt').toString().split('\n')[0])
+let input = Number(require('fs').readFileSync('/dev/stdin').toString().split('\n')[0])
 
-const arr = Array.from({length: input+1}, () => 0)
+const arr = Array.from({ length: input + 1 }, () => 0)
 
-for (let i = 2; i <= input; i++) {
-    let temp = arr[i-1]+1
-    if(i % 3 === 0)
-        temp = Math.min(arr[i / 3]+1, temp)
-    
-    if( i % 2 === 0) 
-        temp = Math.min(arr[i / 2]+1, temp)
-    
-    arr[i] = temp
+arr[2] = 1
+arr[3] = 1
+
+for (let i = 4; i <= input; i++) {
+  let answer = arr[i - 1] + 1
+  if (i % 3 === 0) {
+    answer = Math.min(arr[i / 3] + 1, answer)
+  }
+  if (i % 2 === 0) {
+    answer = Math.min(arr[i / 2] + 1, answer)
+  }
+  arr[i] = answer
 }
 console.log(arr[input])
